42ec6f26f618875a9c643f08d3287835c9ae32a3
ReackMan <reackman@yandex.ru>
2024-04-15 02:08:04 +0300

    add checkbox component

---
+++ src/components/ui/checkbox/checkbox.tsx
@@ -0,0 +1,57 @@
+import { ComponentPropsWithoutRef } from 'react'
+
+import { CheckIcon } from '@/assets'
+import { TypographyVariant } from '@/common'
+import { Typography } from '@/components'
+import * as RadixCheckbox from '@radix-ui/react-checkbox'
+
+import s from './checkbox.module.scss'
+
+type PositionType = 'default' | 'left'
+
+export type CheckboxProps = {
+  checked: boolean
+  id?: string
+  label?: string
+  onCheckedChange: (checked: boolean) => void
+  position?: PositionType
+} & Omit<ComponentPropsWithoutRef<typeof RadixCheckbox.Root>, 'asChild'>
+
+export const Checkbox = ({
+  checked,
+  className,
+  disabled,
+  id,
+  label,
+  name,
+  onCheckedChange,
+  position = 'default',
+  ...restProps
+}: CheckboxProps) => {
+  return (
+    <Typography
+      as={'label'}
+      className={`${s.label} ${disabled && s.disabled} ${className}`}
+      variant={TypographyVariant.Body2}
+    >
+      <div className={`${s.checkboxWrapper} ${disabled && s.disabled} ${s[position]}`}>
+        <RadixCheckbox.Root
+          checked={checked}
+          className={s.root}
+          disabled={disabled}
+          id={id}
+          name={name}
+          onCheckedChange={onCheckedChange}
+          required={restProps.required}
+        >
+          {checked && (
+            <RadixCheckbox.Indicator className={s.indicator} forceMount>
+              <CheckIcon size={1.8} />
+            </RadixCheckbox.Indicator>
+          )}
+        </RadixCheckbox.Root>
+      </div>
+      {label}
+    </Typography>
+  )
+}
