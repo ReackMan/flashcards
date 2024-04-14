34418c8775e631e739a680fdee10d7ad7d45a56e
ReackMan <reackman@yandex.ru>
2024-04-14 04:27:26 +0300

    add polymorphic button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { Button } from './components/ui/button'
+
 export function App() {
-  return <div>Hello</div>
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '12px' }}>
+      <Button>Hello World</Button>
+      <Button as={'a'} href={'https://google.com'}>
+        Hello World
+      </Button>
+    </div>
+  )
 }
