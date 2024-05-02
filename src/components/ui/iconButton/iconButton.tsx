63085c9e8fdd6798c81c9fa30e03d1ee14aac98c
ReackMan <reackman@yandex.ru>
2024-05-02 23:20:13 +0300

    add forwardRef for iconButton

--- src/components/ui/iconButton/iconButton.tsx
+++ src/components/ui/iconButton/iconButton.tsx
@@ -1,9 +1,11 @@
 import {
   CSSProperties,
   ComponentPropsWithoutRef,
+  ElementRef,
   ReactElement,
   ReactNode,
   cloneElement,
+  forwardRef,
   isValidElement,
 } from 'react'
 
@@ -14,17 +16,19 @@ type IconButtonProps = {
   size?: number
 } & ComponentPropsWithoutRef<'button'>
 
-export const IconButton = ({ className, icon, size: sizeProp, ...restProps }: IconButtonProps) => {
-  const size = sizeProp ? `${sizeProp}rem` : '2.4rem'
+export const IconButton = forwardRef<ElementRef<'button'>, IconButtonProps>(
+  ({ className, icon, size: sizeProp, ...restProps }, ref) => {
+    const size = sizeProp ? `${sizeProp}rem` : '2.4rem'
 
-  const IconButtonStyle: CSSProperties = {
-    height: size,
-    width: size,
+    const IconButtonStyle: CSSProperties = {
+      height: size,
+      width: size,
+    }
+
+    return (
+      <button className={`${s.root} ${className}`} ref={ref} style={IconButtonStyle} {...restProps}>
+        {isValidElement(icon) ? cloneElement(icon as ReactElement<any>, { size }) : null}
+      </button>
+    )
   }
-
-  return (
-    <button className={`${s.root} ${className}`} style={IconButtonStyle} {...restProps}>
-      {isValidElement(icon) ? cloneElement(icon as ReactElement<any>, { size }) : null}
-    </button>
-  )
-}
+)
