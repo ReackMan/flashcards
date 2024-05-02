e6a3f9302273ac325c956af6ca88ede1826ad347
ReackMan <reackman@yandex.ru>
2024-05-02 23:23:48 +0300

    add forwardRef for rating

--- src/components/ui/rating/rating.tsx
+++ src/components/ui/rating/rating.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef } from 'react'
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 
 import { RatingEmptyIcon, RatingFilledIcon } from '@/assets'
 
@@ -11,24 +11,20 @@ type RatingProps = {
   size?: number
 } & ComponentPropsWithoutRef<'div'>
 
-export const Rating = ({
-  className,
-  maxRating = 5,
-  rating,
-  size = 1.6,
-  ...restProps
-}: RatingProps) => {
-  const stars = [...Array(maxRating)].map((_, index) => index + 1)
+export const Rating = forwardRef<ElementRef<'div'>, RatingProps>(
+  ({ className, maxRating = 5, rating, size = 1.6, ...restProps }, ref) => {
+    const stars = [...Array(maxRating)].map((_, index) => index + 1)
 
-  return (
-    <div className={`${s.root} ${className}`} {...restProps}>
-      {stars.map((star, index) => {
-        return rating >= star ? (
-          <RatingFilledIcon color={'var(--color-warning-300)'} key={index} size={size} />
-        ) : (
-          <RatingEmptyIcon color={'var(--color-warning-300)'} key={index} size={size} />
-        )
-      })}
-    </div>
-  )
-}
+    return (
+      <div className={`${s.root} ${className}`} ref={ref} {...restProps}>
+        {stars.map((star, index) => {
+          return rating >= star ? (
+            <RatingFilledIcon color={'var(--color-warning-300)'} key={index} size={size} />
+          ) : (
+            <RatingEmptyIcon color={'var(--color-warning-300)'} key={index} size={size} />
+          )
+        })}
+      </div>
+    )
+  }
+)
