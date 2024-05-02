b67e14c74bc21760d8eba0d14f947b99f7b21d37
ReackMan <reackman@yandex.ru>
2024-05-02 23:13:40 +0300

    add utils function queryNotificationHandler

---
+++ src/common/utils/notificationHandler/queryNotificationHandler/queryNotificationHandler.ts
@@ -0,0 +1,20 @@
+import { toast } from 'react-toastify'
+
+import { SerializedError } from '@reduxjs/toolkit'
+import { FetchBaseQueryError } from '@reduxjs/toolkit/query'
+
+export const queryNotificationHandler = (
+  response: FetchBaseQueryError | SerializedError | undefined
+) => {
+  if (response && 'status' in response) {
+    if (typeof response.data === 'object' && response.data && 'message' in response.data) {
+      const errorMessage = response.data.message as string
+
+      toast.error(errorMessage)
+    } else if ('error' in response) {
+      toast.error(response.error)
+    }
+  } else {
+    toast.error('Some error occurred')
+  }
+}
