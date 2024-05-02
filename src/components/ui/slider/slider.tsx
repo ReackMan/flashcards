067536a34bb213bd160855d76fdbe6191df38b14
ReackMan <reackman@yandex.ru>
2024-05-02 23:24:49 +0300

    add forwardRef for slider

--- src/components/ui/slider/slider.tsx
+++ src/components/ui/slider/slider.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef } from 'react'
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 
 import { TypographyVariant } from '@/common'
 import { Typography } from '@/components'
@@ -8,29 +8,31 @@ import s from './slider.module.scss'
 
 export type SliderProps = { label: string } & ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
 
-export const Slider = ({ className, label, ...restProps }: SliderProps) => {
-  return (
-    <div>
-      {label && (
-        <Typography as={'label'} variant={TypographyVariant.Body2}>
-          {label}
-        </Typography>
-      )}
-      <div className={s.container}>
-        <Typography as={'div'} className={s.valueWrapper} variant={TypographyVariant.Body1}>
-          {restProps?.value?.[0]}
-        </Typography>
-        <SliderPrimitive.Root className={`${s.root} ${className}`} {...restProps}>
-          <SliderPrimitive.Track className={s.track}>
-            <SliderPrimitive.Range className={s.range} />
-          </SliderPrimitive.Track>
-          <SliderPrimitive.Thumb aria-label={'Value min'} className={s.thumb} />
-          <SliderPrimitive.Thumb aria-label={'Value max'} className={s.thumb} />
-        </SliderPrimitive.Root>
-        <Typography as={'div'} className={s.valueWrapper} variant={TypographyVariant.Body1}>
-          {restProps?.value?.[1]}
-        </Typography>
+export const Slider = forwardRef<ElementRef<typeof SliderPrimitive.Root>, SliderProps>(
+  ({ className, label, ...restProps }, ref) => {
+    return (
+      <div>
+        {label && (
+          <Typography as={'label'} variant={TypographyVariant.Body2}>
+            {label}
+          </Typography>
+        )}
+        <div className={s.container}>
+          <Typography as={'div'} className={s.valueWrapper} variant={TypographyVariant.Body1}>
+            {restProps?.value?.[0]}
+          </Typography>
+          <SliderPrimitive.Root className={`${s.root} ${className}`} ref={ref} {...restProps}>
+            <SliderPrimitive.Track className={s.track}>
+              <SliderPrimitive.Range className={s.range} />
+            </SliderPrimitive.Track>
+            <SliderPrimitive.Thumb aria-label={'Value min'} className={s.thumb} />
+            <SliderPrimitive.Thumb aria-label={'Value max'} className={s.thumb} />
+          </SliderPrimitive.Root>
+          <Typography as={'div'} className={s.valueWrapper} variant={TypographyVariant.Body1}>
+            {restProps?.value?.[1]}
+          </Typography>
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
+)
